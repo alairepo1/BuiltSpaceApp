e28edf689d48decfffca28aeea8d30ecbf67920b
themissenchanted <suhani.jayatilake@gmail.com>
2020-02-06 14:06:14 -0800

    Revert "Login Screen"
    This reverts commit 97df2740ac8dd5a69bf94a9a4e004e4ecf2273b3.

--- BuiltSpaceApp/index.js
+++ BuiltSpaceApp/index.js
@@ -7,35 +7,3 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-
-import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
-import Splash from './Splash';
-import Login from './src/screens/LoginScreen/Login';
-
-export default class GithubApp extends Component {
-    render() {
-        return (
-            <Login />
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyCOntent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
-
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    }
-})
