b0eaf5a2189946e28ad3cb26ee35ae3d81580a97
zachdg <zachdg645@hotmail.com>
2020-02-07 11:05:53 -0800

    Checklists API fetch working
    Now properly displaying data from checklist fetch

--- BuiltSpaceApp/App.js
+++ BuiltSpaceApp/App.js
@@ -25,7 +25,6 @@ class App extends Component {
   render() {
     return (
       <SafeAreaProvider>
-        {<Text>Hello</Text>}
         <Navigator />
         {/* <RealmDb /> */}
       </SafeAreaProvider>
--- BuiltSpaceApp/src/screens/IsolatedChecklist/AssetScreen.js
+++ BuiltSpaceApp/src/screens/IsolatedChecklist/AssetScreen.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import StatusBar from '../../statusComponent.js';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 export class AssetScreen extends Component {
   constructor(props) {
@@ -9,6 +10,7 @@ export class AssetScreen extends Component {
       Assets: [],
       Checklist: [],
       key: 'GBBNUEFoR1lwQsg/lIyJ5lXcN+ELUowsarB0/HSUl+U=',
+      isloading: true
     };
   
   this.fetch = this.fetch.bind(this);
@@ -26,7 +28,8 @@ export class AssetScreen extends Component {
     .then(results => {
       
       this.setState({
-        Checklist: results
+        Checklist: results,
+        isloading: false
       })
     })
   };
@@ -36,15 +39,32 @@ export class AssetScreen extends Component {
   };
   render () {
       console.log(this.state.Checklist)
-    if (this.state.Checklist) {
+    if (this.state.isloading) {
+      return (
+      <View><Text>Loading</Text></View>
+      );
+    } else {
       return (
         <View style={styles.container}>
-          <Text>Fetch Asset and Checklist API</Text>
-          <Text>Asset Category:
-            {JSON.stringify(this.state.Checklist[3])}</Text>
+          <Text style={styles.header}>Fetch Asset and Checklist API</Text>
+          
+          <Text style={styles.category}>Asset Category:</Text><Text>{JSON.stringify(this.state.Checklist[0].assetCategory)}</Text>
+          <Text style={styles.title}>Asset Title:</Text><Text>{JSON.stringify(this.state.Checklist[0].title)}</Text>
+          <Text style={styles.title}>Asset Id:</Text><Text> {JSON.stringify(this.state.Checklist[0].id)}</Text>
+          
+          <Text style={styles.category}>Asset Category:</Text><Text>{JSON.stringify(this.state.Checklist[1].assetCategory)}</Text>
+          <Text style={styles.title}>Asset Title:</Text><Text>{JSON.stringify(this.state.Checklist[1].title)}</Text>
+          <Text style={styles.title}>Asset Id:</Text><Text> {JSON.stringify(this.state.Checklist[1].id)}</Text>
+
+          <Text style={styles.category}>Asset Category:</Text><Text>{JSON.stringify(this.state.Checklist[2].assetCategory)}</Text>
+          <Text style={styles.title}>Asset Title:</Text><Text>{JSON.stringify(this.state.Checklist[2].title)}</Text>
+          <Text style={styles.title}>Asset Id:</Text><Text> {JSON.stringify(this.state.Checklist[2].id)}</Text>
+          
+          <Text style={styles.category}>Asset Category:</Text><Text>{JSON.stringify(this.state.Checklist[3].assetCategory)}</Text>
+          <Text style={styles.title}>Asset Title:</Text><Text>{JSON.stringify(this.state.Checklist[3].title)}</Text>
+          <Text style={styles.title}>Asset Id:</Text><Text> {JSON.stringify(this.state.Checklist[3].id)}</Text>
         </View>
-      );
-    }
+      )}
   }
 }
 
@@ -55,6 +75,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  category: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  }
 });
 
 export default AssetScreen;
