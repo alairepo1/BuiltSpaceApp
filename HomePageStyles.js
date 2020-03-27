811fa192e5b14b1b4279716dfc2ac71149aa59b9
derekcvdo <47234626+ddo14@users.noreply.github.com>
2020-03-26 18:26:58 -0700

    Added separate style files
    Have to move them to the correct file directories

---
+++ HomePageStyles.js
@@ -0,0 +1,74 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        // flex: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 50,
+      },
+      homePageText: {
+        fontSize: 15,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'black',
+      },
+      button_view: {
+        // flex: 2,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#FAF9ED',
+        marginTop: 15,
+      },
+      button_container: {
+        width: 240,
+        alignSelf: 'center',
+        margin: 10,
+      },
+      buttons: {
+        backgroundColor: '#ABA9A9',
+        margin: 5,
+        color: 'black',
+        height: 50,
+      },
+      button_text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 13,
+      },
+      inspectionContainer: {
+        alignSelf: 'center',
+        width: '100%',
+        margin: 5
+      },
+      inspectionRow: {
+        flex: 2,
+        flexDirection: 'row',
+      },
+      checkbox: {
+        flex: 1
+      },
+      inspectionButtonContainer: {
+        flexDirection: 'row',
+        marginLeft: 15
+      },
+      inspectionName: {
+        flex: 1,
+        fontSize: 15
+      },
+      submit:{
+        width: 100,
+        height: 40,
+        padding: 10,
+        margin: 5,
+        backgroundColor: '#6CD938',
+      },
+      delete:{
+        width: 100,
+        height: 40,
+        padding: 10,
+        margin: 5,
+        backgroundColor: 'red',
+      },
+});
---
+++ LoginStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FAF9ED'
+    },
+    logoContainer: {
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center'
+    },
+    logo: {
+        width: 235,
+        height: 250
+    }
+})
